ef8327e59c3bae68f5360973a3f616c567947ccc
Leonardo <leonardouezu@gmail.com>
2020-11-03 14:39:50 -0300

    add player

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ const ctx = canvas.getContext("2d");
 
 const dotsAr = [];
 
+class Player {
+    constructor(x, y, radius, color){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
+}
+
+const player = new Player(canvas.width / 2, canvas.height / 2, 30, 'black');
+
+console.log(player)
+
+player.draw()
+
 class Dots {
     constructor(x, y, radius, color, velocity){
         this.x = x;
@@ -27,19 +49,25 @@ class Dots {
 }
 function spawn(){
     setInterval(()=>{
-        const x = 100;
-        const y = 100;
         const radius = 30;
-        const color = "green";
+
+        let x = Math.random() * canvas.width;
+        let y = 0;
+
+        const color = "#654321";
+
+        const angle = Math.atan2(canvas.height, canvas.width / 2 - x)
+
         const velocity = {
-            x: 1,
-            y: 1
+            x: Math.cos(angle),
+            y: Math.sin(angle)
         }
         dotsAr.push(new Dots(x, y, radius, color, velocity))
 
         console.log(dotsAr)
-    }, 1000)
+    }, 700)
 }
+
 
 function animate() {
     requestAnimationFrame(animate)
